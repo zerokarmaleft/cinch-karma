8d5aef6781f9409e56e3825cfdff18a6c2ddb1a7
groovecoder <luke.crouch@gmail.com>
2012-01-26 17:15:49 -0600

    <user>?? and karma?? karma queries; refactor points_message; gitignore swap files

--- karma.rb
+++ karma.rb
@@ -3,23 +3,40 @@ require 'cinch'
 class Karma
   include Cinch::Plugin
 
-  match /(\S+)\+{2}/
+  match /((\S+)[\+\?]{2})/
 
   def initialize(*args)
     super
     @users = Hash.new(0)
   end
 
+  def points_message(nick)
+    return "#{ nick } has #{ @users[nick] } awesome points."
+  end
+
   def execute(m)
     cmd = m.params[1]
-    nick = cmd.match(/[!](\S+)[+]{2}/)[1]
+    tokens = cmd.match(/[!](\S+)([\+\?]{2})/)
+    nick = tokens[1]
+    action = tokens[2]
     
-    if nick == @bot.nick
-      m.reply "Increasing my karma would result in overflow."
-    elsif nick == m.user.nick
-      m.reply "Just keep patting yourself on the back there, sport."
-    else
-      m.reply "#{ nick } has #{ @users[nick] += 1 } awesome points."
+    if action == '++'
+      if nick == @bot.nick
+        m.reply "Increasing my karma would result in overflow."
+      elsif nick == m.user.nick
+        m.reply "Just keep patting yourself on the back there, sport."
+      else
+        @users[nick] += 1
+        m.reply points_message(nick)
+      end
+    elsif action == '??'
+      if nick == 'karma'
+        @users.each do |nick, points|
+          m.reply points_message(nick)
+        end
+      else
+        m.reply points_message(nick)
+      end
     end
   end
 end
